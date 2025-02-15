8bb5bea344c120a03ca914fac528e7ba2cbaa55c
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-15 23:03:54 +0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,8 @@
 """
 Test suite for physumber
 """
-import logging
+import sys
+import os
 import pytest
 import physumber.sumbermath
 import physumber.sumberphysics
-
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
