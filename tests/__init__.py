d883bbd9e969fe56ab3314c84732ca0976e55bfa
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-15 22:47:05 +0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,12 @@
 """
 Test suite for physumber
 """
-
+import logging
+import pytest
+import physumber.sumbermath
+import physumber.sumberphysics
 
 import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-import logging
-import pytest  # Now should work
-import physumber.sumbermath  # Now should work
-import physumber.sumberphysics  # Now should work
