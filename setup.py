379a6780ede4c53662936c6fece9e1e8370c25f1
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-15 16:21:23 +0500

    shift to setup.cfg

--- setup.py
+++ setup.py
@@ -1,25 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name = "pyPhysumber",
-    version = "2025.0.0a1",
-    packages = find_packages(),
-    include_package_data = True,
-    install_requires=[],
-    python_requires= ">=3.9" ,
-    author = "Aitzaz Imtiaz",
-    author_email = "aitzazimtiaz.ai@gmail.com",
-    description = "Python library in computing Mathematics and Physics expressions",
-    license = "AGPL-3.0-or-later",
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Programming Language :: Python",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Physics",
-    ],
-    keywords=["Physics", "Mathematics", "Computation"],
-)
+setup()
