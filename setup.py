4411be4bf07bca7e6830de23e4fb6e7980fa3d68
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-22 17:35:22 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 """
 Setup script for Physumber. It assumes to load the setup.cfg and conforms to linter.
 """
+from setuptools import setup, find_packages
 
-from setuptools import setup
-
-setup()
+setup(
+    packages=find_packages(),
+)
