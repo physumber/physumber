bf02e62f1bdfffc72e842a327b8c27797d53e76c
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-22 16:01:15 +0500

    Update test_sumberphysics.py

--- tests/test_sumberphysics.py
+++ tests/test_sumberphysics.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import physumber.sumberphysics 
+
 def test_placeholder_sumberphysics():
     """A placeholder test for sumberphysics module."""
     assert True 
