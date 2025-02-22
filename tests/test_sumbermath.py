c1eb81c25e33fc211c9f3709626ca97923cf7734
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-02-22 16:00:50 +0500

    Update test_sumbermath.py

--- tests/test_sumbermath.py
+++ tests/test_sumbermath.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import physumber.sumbermath 
+
 def test_placeholder_sumbermath():
     """Placeholder."""
     assert True  
