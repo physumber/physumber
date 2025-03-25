a481ce341ac233ca2c02f0873eae71c1362ddbcd
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2025-03-25 15:21:51 +0500

    Update test_algebra.py

--- tests/test_algebra.py
+++ tests/test_algebra.py
@@ -3,7 +3,7 @@ from physumber.sumbermath.algebra import algebra
 
 class TestAlgebra(unittest.TestCase):
     def test_modulus(self):
-        alg = Algebra()
+        alg = algebra()
         self.assertEqual(alg.modulus(-5), 5)
         self.assertEqual(alg.modulus(10), 10)
         self.assertEqual(alg.modulus(0), 0)
